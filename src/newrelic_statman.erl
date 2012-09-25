d6d4f535cf1b29841429364f60b09923d2b17a91
Knut Nesheim <knutin@gmail.com>
2012-09-25 14:53:50 +0200

    Show total time spent in python to get RPM.

--- src/newrelic_statman.erl
+++ src/newrelic_statman.erl
@@ -11,8 +11,11 @@ poll() ->
                    end,
                    Metrics),
 
-      lists:filter(fun (M) -> M =/= [] end,
-                   lists:map(fun transform/1, Histograms)).
+    Ms = lists:filter(
+           fun (M) -> M =/= [] end,
+           lists:map(fun transform/1, Histograms)),
+
+    [add_total(Ms) | Ms].
 
 
 
@@ -40,6 +43,22 @@ transform(Metric) ->
             []
     end.
 
+
+add_total(Ms) ->
+    N    = lists:sum(pluck(1, Ms)),
+    Sum  = lists:sum(pluck(2, Ms)),
+    Min  = lists:min(pluck(4, Ms)),
+    Max  = lists:max(pluck(5, Ms)),
+    Sum2 = lists:sum(pluck(6, Ms)),
+
+    [{[{name, <<"HttpDispatcher">>},
+       {scope, <<"">>}]},
+     [N, Sum, Sum, Min, Max, Sum2]].
+
+pluck(N, L) ->
+    lists:map(fun ([_, []]) -> 0;
+                  ([_, D]) -> lists:nth(N, D)
+              end, L).
 
 
 
